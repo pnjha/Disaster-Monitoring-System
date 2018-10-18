f87271236d46b7bd2b38a9be19da3d05d525406f
Prakash Nath Jha <prakash.nath@students.iiit.ac.in>
2018-10-19 02:08:46 +0530

    changes

--- server.py
+++ server.py
@@ -24,21 +24,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route("/search",methods=["POST"])
+@app.route("/search",methods=['GET','POST'])
 def search():
-	return "<h1>Hello</h1>"
-    # search_tweet = request.form.get("search_query")
-    
-    # # t = [[]]
-    # t = []
-    # tweets = api.search(search_tweet, tweet_mode='extended')
-    # for tweet in tweets:
-    #     polarity = TextBlob(tweet.full_text).sentiment.polarity
-    #     subjectivity = TextBlob(tweet.full_text).sentiment.subjectivity
-    #     t.append([tweet.full_text,polarity,subjectivity])
-    #     # t.append(tweet.full_text)
 
-    # return jsonify({"success":True,"tweets":t})
+    search_tweet = request.form.get("search_query")
+    
+    # t = [[]]
+    t = []
+    tweets = api.search(search_tweet, tweet_mode='extended')
+    for tweet in tweets:
+        polarity = TextBlob(tweet.full_text).sentiment.polarity
+        subjectivity = TextBlob(tweet.full_text).sentiment.subjectivity
+        t.append([tweet.full_text,polarity,subjectivity])
+        # t.append(tweet.full_text)
+
+    return jsonify({"success":True,"tweets":t})
 
 if __name__ == '__main__' : 
 	app.run(debug=True)
