5146e584077e53d0a2ed6d894445047b7f308e85
Prakash Nath Jha <prakash.nath@students.iiit.ac.in>
2018-10-19 00:22:08 +0530

    changes

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         // Add data to send with request
 
-        const data = new FormData();
+        let data = new FormData();
         data.append('search_query', search_query);
         console.log(data);
         // Send request
