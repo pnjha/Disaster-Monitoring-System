6324cf5c8463bafa0e50d995437737e9fe29be49
Prakash Nath Jha <prakash.nath@students.iiit.ac.in>
2018-10-18 19:36:29 +0530

    changes

--- application.py
+++ application.py
@@ -10,26 +10,27 @@
 
 
 
-from flask import Flask,render_template,request,jsonify
-import tweepy
-from textblob import TextBlob
+from flask import Flask
+# ,render_template,request,jsonify
+# import tweepy
+# from textblob import TextBlob
 
 
-consumer_key = 'HcWuxEn7uMvsBth3z9oBNBWkQ'
-consumer_secret = 'f5rM7IDFQF4yGCWa1VTvrOCukuHl3DPyeBw38E6hthiTRFQnxi'
+# consumer_key = 'HcWuxEn7uMvsBth3z9oBNBWkQ'
+# consumer_secret = 'f5rM7IDFQF4yGCWa1VTvrOCukuHl3DPyeBw38E6hthiTRFQnxi'
 
-access_token = '3065963708-IxpW6kwNtbp77qTHV6lCpq4MLny5LF3N23XlQfN'
-access_token_secret = '8shltRS8a7DjjNd7IK5Pc2AYiSQvBzC7TIiUb4gC1SZON'
+# access_token = '3065963708-IxpW6kwNtbp77qTHV6lCpq4MLny5LF3N23XlQfN'
+# access_token_secret = '8shltRS8a7DjjNd7IK5Pc2AYiSQvBzC7TIiUb4gC1SZON'
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+# auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+# auth.set_access_token(access_token, access_token_secret)
 
-api = tweepy.API(auth)
+# api = tweepy.API(auth)
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def index():
 	return render_template('index.html')
 
 @app.route("/search",methods=["POST"])
