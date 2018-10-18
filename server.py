2b59fcf2eee882cdc4faf8ab664157d4b68123b3
Prakash Nath Jha <prakash.nath@students.iiit.ac.in>
2018-10-19 01:50:34 +0530

    changes

--- server.py
+++ server.py
@@ -26,19 +26,19 @@ def index():
 
 @app.route("/search",methods=["POST"])
 def search():
+	return "<h1>Hello</h1>"
+    # search_tweet = request.form.get("search_query")
+    
+    # # t = [[]]
+    # t = []
+    # tweets = api.search(search_tweet, tweet_mode='extended')
+    # for tweet in tweets:
+    #     polarity = TextBlob(tweet.full_text).sentiment.polarity
+    #     subjectivity = TextBlob(tweet.full_text).sentiment.subjectivity
+    #     t.append([tweet.full_text,polarity,subjectivity])
+    #     # t.append(tweet.full_text)
 
-    search_tweet = request.form.get("search_query")
-    search_tweet = "hyderabad"
-    # t = [[]]
-    t = []
-    tweets = api.search(search_tweet, tweet_mode='extended')
-    for tweet in tweets:
-        polarity = TextBlob(tweet.full_text).sentiment.polarity
-        subjectivity = TextBlob(tweet.full_text).sentiment.subjectivity
-        t.append([tweet.full_text,polarity,subjectivity])
-        # t.append(tweet.full_text)
-
-    return jsonify({"success":True,"tweets":t})
+    # return jsonify({"success":True,"tweets":t})
 
 if __name__ == '__main__' : 
 	app.run(debug=True)
