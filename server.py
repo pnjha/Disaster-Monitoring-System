5f7cab5140dfa351fea7cb95946144346421d258
Prakash Nath Jha <prakash.nath@students.iiit.ac.in>
2018-10-19 23:22:11 +0530

    changes

--- server.py
+++ server.py
@@ -24,21 +24,28 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route("/search",methods=['GET','POST'])
-def search():
 
-    search_tweet = request.form.get("search_query")
+@app.route('/search',methods=['GET','POST'])
+def show_account():
+	abort(401)
+	return "balance is ..."
+
+
+# @app.route("/search",methods=['GET','POST'])
+# def search():
+
+#     search_tweet = request.form.get("search_query")
     
-    # t = [[]]
-    t = []
-    tweets = api.search(search_tweet, tweet_mode='extended')
-    for tweet in tweets:
-        polarity = TextBlob(tweet.full_text).sentiment.polarity
-        subjectivity = TextBlob(tweet.full_text).sentiment.subjectivity
-        t.append([tweet.full_text,polarity,subjectivity])
-        # t.append(tweet.full_text)
+#     # t = [[]]
+#     t = []
+#     tweets = api.search(search_tweet, tweet_mode='extended')
+#     for tweet in tweets:
+#         polarity = TextBlob(tweet.full_text).sentiment.polarity
+#         subjectivity = TextBlob(tweet.full_text).sentiment.subjectivity
+#         t.append([tweet.full_text,polarity,subjectivity])
+#         # t.append(tweet.full_text)
 
-    return jsonify({"success":True,"tweets":t})
+#     return jsonify({"success":True,"tweets":t})
 
 if __name__ == '__main__' : 
 	app.run(debug=True)
